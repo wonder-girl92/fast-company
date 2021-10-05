4d992821ce2973ce3c19e8053707461e4b521d13
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2021-10-05 15:32:21 +0300

    get users

---
+++ src/components/users.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import api from '../api'
+
+function Users (props) {
+  const [users, setUsers] = useState(api.users.fetchAll())
+
+  // let counterClasses = 'badge bg-'
+
+  let spanStyles = {
+    fontSize: '25px',
+    }
+
+  return (<>
+    <span style={spanStyles} className="badge bg-primary mb-3 mt-2">
+      тусанет с тобой сегодня
+    </span>
+    <table className="table">
+      <thead>
+      <tr>
+        <th scope="col">Имя</th>
+        <th scope="col">Качества</th>
+        <th scope="col">Профессия</th>
+        <th scope="col">Встретился, раз</th>
+        <th scope="col">Оценка</th>
+        <th scope="col">Удалить</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        {users.map((user) => {
+          return (
+            <td key={user._id}>
+              {user.name}
+            </td>
+          )
+        })}
+        <td></td>
+        <td></td>
+        <td>
+          <button className="btn btn-danger btn-sm">
+            delete
+          </button>
+        </td>
+      </tr>
+      {/*    {users.map((user) => {*/}
+      {/*  return (*/}
+      {/*      <tr key={user._id}>*/}
+      {/*        {user.qualities.map((qual)=>{*/}
+      {/*          return <td key={qual._id}> {qual} </td>*/}
+      {/*        })}*/}
+      {/*      </tr>*/}
+      {/*  )*/}
+      {/*})}*/}
+
+      </tbody>
+    </table>
+
+  </>)
+}
+
+export default Users
